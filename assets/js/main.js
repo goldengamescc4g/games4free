f01f5cf9f8348c847e08a26368597040516d7af8
Altamiro Bruno <62890135+brunobarross@users.noreply.github.com>
2022-02-08 08:51:47 -0300

    melhorias

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,8 @@
 import ajaxConteudo from "./modules/ajaxConteudo.js";
 import ajaxModal from "./modules/ajaxModal.js";
 import filtroMobile from "./modules/filtroMobile.js";
-import translateContent from "./modules/translateContent.js";
+
 
 await ajaxConteudo();
 await ajaxModal();
 await filtroMobile();
-// await translateContent();
